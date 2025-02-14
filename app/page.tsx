60deedb9c218c22e5e570c73c632f33eb0e3828e
RelentlessCore <dardankabashi101@gmail.com>
2025-02-14 14:28:34 +0100

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import PatientForm from "@/components/forms/PatientForm";
+import PasskeyModal from "@/components/PasskeyModal";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+export default function Home({ searchParams }: SearchParamProps) {
+  const isAdmin = searchParams.admin === "true";
   return (
+<<<<<<< Updated upstream
     <div className="flex h-screen max-h-screen"> 
       {/* TODO: OTP Verification | PasskeyModal */} 
  
@@ -18,15 +21,31 @@ export default function Home() {
           />
 
         <PatientForm /> 
+=======
+    <div className="flex h-screen max-h-screen">
+      {isAdmin && <PasskeyModal />}
 
-        <div className="text-14-regular mt-20 flex justify-between">
-          <p className="justify-items-end text-dark-600 xl:text-left">
-            © 2024 CarePulse 
-          </p>
-          <Link href="/?admin=true" className="text-green-500">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="patient"
+            className="mb-12 h-10 w-fit"
+          />
+
+          <PatientForm />
+>>>>>>> Stashed changes
+
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2024 CarePulse
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
               Admin
             </Link>
-        </div>
+          </div>
         </div>
       </section> 
 
@@ -38,5 +57,9 @@ export default function Home() {
         className="side-img max-w-[50%]" 
       />  
     </div>
+<<<<<<< Updated upstream
   ) 
+=======
+  );
+>>>>>>> Stashed changes
 }
