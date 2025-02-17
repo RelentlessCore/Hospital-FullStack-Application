5ef4174bc314aab4424a0d24dc64eea7f4040e46
RelentlessCore <dardankabashi101@gmail.com>
2025-02-17 23:43:07 +0100

    commit

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,13 +1,13 @@
-import DataTable from "@/components/table/DataTable";
+import { DataTable } from "@/components/table/DataTable";
 import StatCard from "@/components/StatCard";
 import { getRecentAppointmentList } from "@/lib/actions/appointment.actions";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
-import { columns } from "@/components/table/columns";
+import { columns, Payment } from "@/components/table/columns";
 
 const Admin = async () => {
   const appointments = await getRecentAppointmentList();
+
   return (
     <div className="mx-auto flex max-w-7xl flex-col space-y-14">
       <header className="admin-header">
--- app/page.tsx
+++ app/page.tsx
@@ -6,24 +6,8 @@ import Link from "next/link";
 export default function Home({ searchParams }: SearchParamProps) {
   const isAdmin = searchParams.admin === "true";
   return (
-<<<<<<< Updated upstream
-    <div className="flex h-screen max-h-screen"> 
-      {/* TODO: OTP Verification | PasskeyModal */} 
- 
-      <section className="remove-scrollbar container my-auto">
-        <div className="sub-container max-w-[496px]">
-          <Image 
-          src="/assets/icons/logo-full.svg"
-          height={1000}
-          width={1000}  
-          alt="patient" 
-          className="mb-12 h-10 w-fit"
-          />
-
-        <PatientForm /> 
-=======
     <div className="flex h-screen max-h-screen">
-      {isAdmin && <PasskeyModal />}
+      {/* TODO: OTP Verification | PasskeyModal */}
 
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
@@ -36,7 +20,6 @@ export default function Home({ searchParams }: SearchParamProps) {
           />
 
           <PatientForm />
->>>>>>> Stashed changes
 
           <div className="text-14-regular mt-20 flex justify-between">
             <p className="justify-items-end text-dark-600 xl:text-left">
@@ -47,19 +30,15 @@ export default function Home({ searchParams }: SearchParamProps) {
             </Link>
           </div>
         </div>
-      </section> 
+      </section>
 
       <Image
         src="/assets/images/onboarding-img.png"
-        height={1000} 
+        height={1000}
         width={1000}
         alt="patient"
-        className="side-img max-w-[50%]" 
-      />  
+        className="side-img max-w-[50%]"
+      />
     </div>
-<<<<<<< Updated upstream
-  ) 
-=======
   );
->>>>>>> Stashed changes
 }
