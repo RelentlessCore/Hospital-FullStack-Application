8e72f029f0ec233c271de2257c13e179aa8af0c3
Relentless <dardankabashi101@gmail.com>
2025-02-18 02:43:09 +0100

    Update AppointmentForm.tsx

--- components/forms/AppointmentForm.tsx
+++ components/forms/AppointmentForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { zodResolver } from "@hookform/resolvers/zod";
+import { zodResolver } from "@hookform/resolvers/zod"; 
 import Image from "next/image";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
