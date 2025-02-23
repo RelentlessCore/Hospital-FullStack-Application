29d25ca6e04045027fbe9c7ee9b7fead4c419831
Relentless <dardankabashi101@gmail.com>
2025-02-23 23:41:55 +0100

    Update validation.ts

--- lib/validation.ts
+++ lib/validation.ts
@@ -13,7 +13,7 @@ export const UserFormValidation = z.object({
 
 export const PatientFormValidation = z.object({ 
   name: z
-    .string()
+    .string() 
     .min(2, "Name must be at least 2 characters")
     .max(50, "Name must be at most 50 characters"),
   email: z.string().email("Invalid email address"),
